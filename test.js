e38aa90e29d9954570b5a5cc82d3ccf9dd03f22e
CiBotTA <34044699+CiBotTA@users.noreply.github.com>
2017-12-03 15:13:07 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,17 @@
-// const isBuildSuccess = require("./build.config");
-// var assert = require('assert');
+const isBuildSuccess = require("./build.config");
+var assert = require('assert');
 
-// describe('Array', function() {
-//   describe('#indexOf()', function() {
-//     it('should return -1 when the value is not present', function(){
-//       assert.equal(-1, [1,2,3].indexOf(4));
-//     });
-//   });
-// });
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
 
-// describe("Build Fail or Success", function(){
-//   it('should return true if build is success', function(){
-//     assert.equal(true, isBuildSuccess);
-//   })
+describe("Build Fail or Success", function(){
+  it('should return true if build is success', function(){
+    assert.equal(true, isBuildSuccess);
+  })
   
-// });
-//
+});
